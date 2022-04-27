f257b1619773f8536c182a4ca17f6bc75739eda8
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-04-27 13:42:19 -0300

    tela de carrinho

--- VeganHouse/app/src/main/java/com/example/veganhouse/MainActivity.kt
+++ VeganHouse/app/src/main/java/com/example/veganhouse/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.veganhouse
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
     
     fun showMenu(v:View){
