43896220294c9f9b8d6ebd7b8f12bc55ecd474b9
Miguel Nunes <miguel.assuncao@linx3.com>
2022-04-27 14:00:25 -0300

    vh-00-payment - Adiconado tela de pagamento

--- VeganHouse/app/src/main/java/com/example/veganhouse/MainActivity.kt
+++ VeganHouse/app/src/main/java/com/example/veganhouse/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.veganhouse.fragments.HomeFragment
 import com.example.veganhouse.fragments.LoginFragment
+import com.example.veganhouse.fragments.PaymentFragment
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -17,12 +18,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val homeFragment = HomeFragment()
         val loginFragment = LoginFragment()
+        val paymentFragment = PaymentFragment()
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
         makeCurrentFragment(homeFragment)
 
         bottomNavigationView.setOnNavigationItemSelectedListener { item -> when(item.itemId){
             R.id.icon_home -> makeCurrentFragment(homeFragment);
             R.id.icon_user -> makeCurrentFragment(loginFragment)
+            R.id.icon_shopping_bag -> makeCurrentFragment(paymentFragment)
         }
         true
         }
